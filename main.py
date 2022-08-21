ef1d7dd61a38f8dd711c57001ba1f9e228ae818b
lucasshibata <104270159+lucasshibata@users.noreply.github.com>
2022-08-21 13:29:48 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+GUI = Builder.load_file("tela.kv")
+
+class MeuAplicativo(App):
+    def build(self):
+        return GUI
+
+if __name__ == "__main__":
+    MeuAplicativo().run()
